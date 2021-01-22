be8617f91ce23301f6e2ccd3c9ce9675cb2ee3df
hjban <baneju212@gmail.com>
2021-01-22 19:03:31 +0900

    라우터 생성

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SkillsPage from "./pages/SkillsPage";
 import Home from "./pages/Home";
 import ProjectsPage from "./pages/ProjectsPage";
+import BlogPage from "./pages/BlogPage";
+import ProjectDetail from "./pages/ProjectDetail";
+
 function App() {
 	return (
 		<Router>
@@ -10,6 +13,8 @@ function App() {
 				<Route exact path="/" component={Home} />
 				<Route exact path="/skills" component={SkillsPage} />
 				<Route exact path="/projects" component={ProjectsPage} />
+				<Route exact path="/projects/:name" component={ProjectDetail} />
+				<Route exact path="/blog" component={BlogPage} />
 			</Switch>
 		</Router>
 	);
